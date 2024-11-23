71d64403d634f8cab3fd4505b13c1741060b5ea4
Ikenna Anikwe <31927515+iyke0z@users.noreply.github.com>
2024-11-23 12:46:57 +0100

    fix

--- src/javascript/Api/Api.js
+++ src/javascript/Api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let Api = axios.create({
     // baseURL: "http://bijou.test/api/v1",
     // baseURL: "http://localhost:9005/api/v1",
-    baseURL: "https://api.edzain.ngmkt.site/api/v1",
+    baseURL: "https://api.chosenconcept.ngmkt.site/api/v1",
     headers: {
         Authorization: 'Bearer ' + localStorage.getItem('token')
     },
