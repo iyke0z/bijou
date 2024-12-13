af30157c6c48dc916c1bcb51b7a55cf6762640b6
Ikenna Anikwe <31927515+iyke0z@users.noreply.github.com>
2024-12-13 04:38:53 +0100

    fix

--- src/javascript/Api/Api.js
+++ src/javascript/Api/Api.js
@@ -1,19 +1,7 @@
 import axios from 'axios'
 
 let Api = axios.create({
-    // baseURL: "http://localhost:9009/api/v1",
-    // baseURL: "https://api.aromaperk.ngmkt.site/api/v1",
-    // baseURL: "https://api.bingoml.ngmkt.site/api/v1",
-    baseURL: "https://api.edzain.ngmkt.site/api/v1",
-    // baseURL: "https://api.fitandfeet.ngmkt.site/api/v1",
-    // baseURL: "https://api.totech.ngmkt.site/api/v1",
-    // baseURL: "https://api.chosenconcept.ngmkt.site/api/v1",
-    // baseURL: "https://api.bethany.ngmkt.site/api/v1",
-    // baseURL: "https://api.royalrelish.ngmkt.site/api/v1",
-    // baseURL: "https://api.jtscorner.ngmkt.site/api/v1",
-    // baseURL: "https://api.phlorah.ngmkt.site/api/v1",
-    // baseURL: "https://api.amasemporium.ngmkt.site/api/v1",
-
+    baseURL: "http://localhost:9009/api/v1",
     headers: {
         Authorization: 'Bearer ' + localStorage.getItem('token')
     },
